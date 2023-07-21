f0673e917ec63a7eed75dd4c1e87204a050b5268
Robert <robertdev95@gmail.com>
2023-07-21 17:04:07 -0400

    Reuse blog list to filter Mario's blogs

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
+  const [blogs, setBlogs] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
+  
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
 
-    return ( 
-        <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" />
-        </div>
-     );
+  useEffect(() =>{
+    console.log('use effect ran');
+  });
+
+  return ( 
+      <div className="home">
+        <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+        <BlogList blogs= {blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs"/>
+      </div>
+    );
 }
  
 export default Home;
