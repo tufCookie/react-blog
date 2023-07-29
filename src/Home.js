b214fbab636a6823c1a64bf6561d0b4b180908b7
Robert <robertdev95@gmail.com>
2023-07-29 01:04:24 -0400

    Add error handling
    This commit handles errors
    when fetching the data results in an error.
    If you set the fetch url to http://localhost:8000/bloggs
    then you should see an error.

--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,7 @@ const Home = () => {
 
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   const [name, setName] = useState('mario');
   
@@ -13,16 +14,26 @@ const Home = () => {
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
       .then(res => {
+        if (!res.ok) {
+          throw Error('Could not load data')
+        }
         return res.json();
       })
       .then(data => {
         setBlogs(data);
         setIsPending(false);
+        setError(null);
       })
+      .catch(e =>
+      {
+        setIsPending(false);
+        setError(e.message)
+      });
   }, []);
 
   return ( 
     <div className="home">
+      { error && <div>{ error }</div>}
       { isPending && <div>Loading...</div>}
       { blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
