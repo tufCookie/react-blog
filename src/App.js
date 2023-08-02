1d144dc567fe91dc31c1b575d20e0fab629d3147
Robert <robertdev95@gmail.com>
2023-08-01 23:24:34 -0400

    Added routing for home and create

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+import Create from './Create';
+
+
 
 function App() {
   const title = 'Welcome to the new blog';
-
+  
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
-      </div>
-    </div>
+    <Router>
+        <div className="App">
+          <Navbar />
+        <div className="content">
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/create' element={<Create/>}/>
+          </Routes>
+        </div>
+        </div>
+      </Router>
   );
 }
 
