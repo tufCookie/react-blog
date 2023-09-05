dac7bd23bec7637308d79b182abff38e842ab33d
Robert <robertdev95@gmail.com>
2023-09-04 22:34:53 -0400

    Add useEffect method to cleanup

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,28 +1,36 @@
 import { useState, useEffect } from 'react';
 
 const useFetch = (url) => {
+   
     const [data, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-    fetch(url)
-    .then(res => {
-        if (!res.ok) {
-        throw Error('Could not load data')
-        }
-        return res.json();
-    })
-    .then(data => {
-        setBlogs(data);
-        setIsPending(false);
-        setError(null);
-    })
-    .catch(e =>
-    {
-        setIsPending(false);
-        setError(e.message)
-    });
+        const abortCont = new AbortController();
+        fetch(url, {signal: abortCont.signal})
+        .then(res => {
+            if (!res.ok) {
+            throw Error('Could not load data')
+            }
+            return res.json();
+        })
+        .then(data => {
+            setBlogs(data);
+            setIsPending(false);
+            setError(null);
+        })
+        .catch(e =>
+        {
+            if (e.name === 'AbortError') {
+                console.log('fetch aborted');
+            }
+            else {
+                setIsPending(false);
+                setError(e.message)
+            }
+        }, 1000);
+        return () => abortCont.abort();
     }, [url]);
 
     return { data, isPending, error }
